0d6c2768521286e17dfea34e294de188c0f736d6
Bryan Gopal <bryangopal@gmail.com>
2021-03-02 22:00:57 -0700

    changed bs and removed flag

--- utils/parsing.py
+++ utils/parsing.py
@@ -19,10 +19,9 @@ _parser.add_argument("--ds_path", default=None, type=str)
 _parser.add_argument("--ds_encoder", type=str, default="resnet50",
                      choices=["resnet50", "resnet18_bn"])   
 _parser.add_argument("--ds_lr", default=2e-4, type=float)
-_parser.add_argument("--single_lead", action="store_true")
 
 _parser.add_argument("--fast_dev_run", action="store_true")
 _parser.add_argument("--crop_time", type=int, default=5)
-_parser.add_argument("--batch_size", type=float, default=1024)
+_parser.add_argument("--batch_size", type=float, default=2048)
 
 args = _parser.parse_args()
