4a207289090dbedc5cb3cf453430f7ff8de00376
Bryan Gopal <bryangopal@gmail.com>
2021-03-06 13:03:22 -0700

    changed default encoder

--- utils/parsing.py
+++ utils/parsing.py
@@ -14,8 +14,8 @@ _parser.add_argument("--z_dim", default=256, type=int)
 
 _parser.add_argument("--skip_ds", action="store_true")
 _parser.add_argument("--ds_path", default=None, type=str)
-_parser.add_argument("--ds_encoder", type=str, default="resnet50",
-                     choices=["resnet50", "resnet18_bn"])   
+_parser.add_argument("--ds_encoder", type=str, default="resnet18",
+                     choices=["resnet50", "resnet18"])   
 _parser.add_argument("--ds_lr", default=2e-4, type=float)
 
 _parser.add_argument("--fast_dev_run", action="store_true")
