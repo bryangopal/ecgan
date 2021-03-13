9727e3c5d5bf8ac35f22e8ea2a595b52d1b41149
Bryan Gopal <bryangopal@gmail.com>
2021-03-12 22:02:51 -0700

    updated paths

--- utils/paths.py
+++ utils/paths.py
@@ -6,4 +6,5 @@ data_path = f"{general_path}/data"
 raw_data_path = f"{data_path}/raw"
 
 main_dir = args.ds_gen_mode if args.ds_gen_mode else "gan"
-check_dir = f"{general_path}/checkpoints/{args.frac}/{main_dir}"
+co_dir = f"-{args.ds_gen_frac}" if args.ds_gen_mode == "augment" else ""
+check_dir = f"{general_path}/checkpoints/{args.gan_data_frac}/{main_dir}{co_dir}"
