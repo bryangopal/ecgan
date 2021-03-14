2cd860c705ceb7d1c3383ffa534af07ac3cba4d4
Bryan Gopal <bryangopal@gmail.com>
2021-03-13 22:52:20 -0700

    made gen_samples independent

--- gen_samples.py
+++ gen_samples.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
 __author__ = "Bryan Gopal"
 
+from argparse import ArgumentParser, Namespace
 from pytorch_lightning import seed_everything
+import os
 from systems import ECGAN
 import torch
-from utils import configs, classes, check_dir
+import numpy as np
 
-def main():
+classes = np.array(sorted(("270492004", "164889003", "164890007", "426627000", 
+                           "713427006", "713426002", "445118002", "39732003" , 
+                           "164909002", "251146004", "698252002", "10370003" , 
+                           "284470004", "427172004", "164947007", "111975006", 
+                           "164917005", "47665007" , "427393009", "426177001", 
+                           "426783006", "427084000", "164934002", "59931005"  )))
+
+def main(args: Namespace):
   seed_everything(6)
-  cfg = configs.gan
-  if not cfg.path: raise ValueError("Sample Generation Requires a Saved GAN.")
+  if not args.path: raise ValueError("Sample Generation Requires a Saved GAN.")
 
-  gan = ECGAN.load_from_checkpoint(cfg.path)
+  gan = ECGAN.load_from_checkpoint(args.path)
   age = torch.tensor([[0.5]])
   sex = torch.tensor([[0]])
-  dx  = torch.from_numpy(classes == "426783006").float().unsqueeze_(0) #atrial fibrilation
+  dx  = torch.from_numpy(classes == "426783006").float().unsqueeze_(0)
 
-  torch.save(gan(age, sex, dx), f"{check_dir}/58_F_SINUS.pt")
+  torch.save(gan(age, sex, dx), f"{os.path.split(args.path)[0]}/58_F_SINUS.pt")
 
 if __name__ == "__main__":
-  main()
+  _parser = ArgumentParser()
+
+  _parser.add_argument("gan_path", type=os.path.expanduser)
+  main(_parser.parse_args())
