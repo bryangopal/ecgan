84264d3cd2b39964fae152e97c1c06f2ec6fa48a
Bryan Gopal <bryangopal@gmail.com>
2021-03-13 23:11:50 -0700

    Update gen_samples.py

--- gen_samples.py
+++ gen_samples.py
@@ -2,6 +2,7 @@
 __author__ = "Bryan Gopal"
 
 from argparse import ArgumentParser, Namespace
+from numpy.core.fromnumeric import size
 from pytorch_lightning import seed_everything
 import os
 from systems import ECGAN
@@ -19,8 +20,8 @@ def main(args: Namespace):
   seed_everything(6)
 
   gan = ECGAN.load_from_checkpoint(args.path)
-  age = torch.tensor([[0.5]])
-  sex = torch.tensor([[0]])
+  age = torch.rand(size=(1, 1))
+  sex = torch.randint(high=2, size=(1, 1))
   dx  = torch.from_numpy(classes == "426783006").float().unsqueeze_(0)
 
   torch.save(gan(age, sex, dx), f"{os.path.split(args.path)[0]}/58_F_SINUS.pt")
