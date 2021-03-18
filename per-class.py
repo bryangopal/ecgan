0780e56a987f7a50efa378da42a34f1ba180a0f0
Bryan Gopal <bryangopal@gmail.com>
2021-03-17 20:57:51 -0700

    update

--- per-class.py
+++ per-class.py
@@ -6,42 +6,30 @@ from dataprocessing.datamodule import PhysionetDataModule
 from torch.nn import functional as F
 from torch import Tensor
 from pytorch_lightning import seed_everything
-from pytorch_lightning.metrics.functional import auroc
+from pytorch_lightning.metrics import AUROC
 import os
 from systems import Classifier
 import torch
 import numpy as np
 from utils import configs, num_classes, class_labels, classes
-
-def multiclass_auroc(input: Tensor, target: Tensor, print_classes: bool = False) -> Tensor:
-  with torch.no_grad():
-    pred = F.softmax(input, dim=-1)
-
-    aurocs = {}
-    for c in range(num_classes):
-      if 1 in target[:, c] and 0 in target[:, c]:
-        aurocs[class_labels[classes[c]]] = auroc(pred[:, c], target[:, c])
-
-    if not aurocs: raise ValueError("No positive labels found in target")
-    if print_classes: print(aurocs)
-    return torch.mean(torch.tensor(tuple(aurocs.values())))
-
 def main():
   seed_everything(6)
 
   classifier = Classifier.load_from_checkpoint(configs.ds.path)
+  auroc = AUROC(num_classes, compute_on_step=False, average=None)
   pdm = PhysionetDataModule(**configs.pdm)
   pdm.setup("test")
 
-  input = torch.empty(0)
-  target = torch.empty(0, dtype=torch.long)
   for x, *_, y in pdm.test_dataloader():
     with torch.no_grad():
-      input = torch.cat((input, classifier(x)), dim=0)
-      target = torch.cat((target, y.long()))
+     auroc(F.softmax(classifier(x), dim=-1), y.long())
   
-  multiclass_auroc(input, target, True)
-
+  x = auroc.compute()
+  aucs = {}
+  for c in range(num_classes):
+    aucs[class_labels[classes[c]]] = x[c]
+  
+  print(aucs)
   
 
 if __name__ == "__main__":
